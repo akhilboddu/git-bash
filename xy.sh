f0c94c71dc0ab599686bdeb097714e704b8eef72
Akhil Boddu <akhil1234mara@gmail.com>
2020-07-25 18:19:20 +0300

    added Z

--- xy.sh
+++ xy.sh
@@ -4,6 +4,7 @@
 # take 2 inputs x and y
 read -p "Enter X: " x;
 read -p "Enter Y: " y;
+read -p "Enter Z: " z;
 
 #check if x is less than y - output "X is less than Y"
 # else output "Y is less than X"
