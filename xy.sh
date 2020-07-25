185c0d854cfaa0aed3fd64e28816bd33e0a38692
Akhil Boddu <akhil1234mara@gmail.com>
2020-07-25 18:24:50 +0300

    a has been added

--- xy.sh
+++ xy.sh
@@ -2,9 +2,11 @@
 #akhil boddu
 
 # take 2 inputs x and y
+read -p "Enter A: " a;
 read -p "Enter X: " x;
 read -p "Enter Y: " y;
 read -p "Enter Z: " z;
+
 
 #check if x is less than y - output "X is less than Y"
 # else output "Y is less than X"
